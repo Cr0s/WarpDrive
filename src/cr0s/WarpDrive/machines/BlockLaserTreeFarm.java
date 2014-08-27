a026367d3ca2e7afa31f353ef8c3c5ead0dacade
LemADEC <lemadec.fr@gmail.com>
2014-08-27 16:40:13 +0200

    Let's merge...

--- src/cr0s/WarpDrive/machines/BlockLaserTreeFarm.java
+++ src/cr0s/WarpDrive/machines/BlockLaserTreeFarm.java
@@ -1,61 +1,58 @@
-package cr0s.WarpDrive;
+package cr0s.WarpDrive.machines;
 
-import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import cr0s.WarpDrive.WarpDrive;
+
 import java.util.Random;
+
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 
-public class BlockMiningLaser extends BlockContainer
-{
+public class BlockLaserTreeFarm extends BlockContainer {
     private Icon[] iconBuffer;
 
-    private final int ICON_SIDE = 0;
-
-    public BlockMiningLaser(int id, int texture, Material material)
-    {
+    public BlockLaserTreeFarm(int id, int texture, Material material) {
         super(id, material);
+        setHardness(0.5F);
+		setStepSound(Block.soundMetalFootstep);
+		setCreativeTab(WarpDrive.warpdriveTab);
+		setUnlocalizedName("warpdrive.machines.LaserTreeFarm");
     }
 
     @Override
     @SideOnly(Side.CLIENT)
-    public void registerIcons(IconRegister par1IconRegister)
-    {
+    public void registerIcons(IconRegister par1IconRegister) {
         iconBuffer = new Icon[2];
         // Solid textures
         iconBuffer[0] = par1IconRegister.registerIcon("warpdrive:particleBoosterTopBottom");
-        iconBuffer[1] = par1IconRegister.registerIcon("warpdrive:miningLaserSide0");
+        iconBuffer[1] = par1IconRegister.registerIcon("warpdrive:laserTreeFarmSide0");
     }
 
     @Override
-    public Icon getIcon(int side, int metadata)
-    {
-        if (side == 0 || side == 1)
-        {
+    public Icon getIcon(int side, int metadata) {
+        if (side == 0 || side == 1) {
             return iconBuffer[0];
         }
 
-        return iconBuffer[metadata + 1];
+        return iconBuffer[1];
     }
-
+    
     @Override
-    public TileEntity createNewTileEntity(World var1)
-    {
-        return new TileEntityMiningLaser();
+    public TileEntity createNewTileEntity(World var1) {
+        return new TileEntityLaserTreeFarm();
     }
 
     /**
      * Returns the quantity of items to drop on block destruction.
      */
     @Override
-    public int quantityDropped(Random par1Random)
-    {
+    public int quantityDropped(Random par1Random) {
         return 1;
     }
 
@@ -63,8 +60,7 @@ public class BlockMiningLaser extends BlockContainer
      * Returns the ID of the items to drop on destruction.
      */
     @Override
-    public int idDropped(int par1, Random par2Random, int par3)
-    {
+    public int idDropped(int par1, Random par2Random, int par3) {
         return this.blockID;
     }
 }
--- src/cr0s/WarpDrive/machines/BlockWarpIsolation.java
+++ src/cr0s/WarpDrive/machines/BlockWarpIsolation.java
@@ -1,38 +1,36 @@
-package cr0s.WarpDrive;
+package cr0s.WarpDrive.machines;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import cr0s.WarpDrive.WarpDrive;
 
 import java.util.Random;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Icon;
-import net.minecraft.world.World;
 
-public class BlockWarpIsolation extends Block
-{
+public class BlockWarpIsolation extends Block {
     private Icon[] iconBuffer;
 
-    public BlockWarpIsolation(int id, int texture, Material material)
-    {
+    public BlockWarpIsolation(int id, int texture, Material material) {
         super(id, material);
+        setHardness(0.5F);
+		setStepSound(Block.soundMetalFootstep);
+		setCreativeTab(WarpDrive.warpdriveTab);
+		setUnlocalizedName("warpdrive.blocks.WarpIsolation");
     }
 
     @Override
     @SideOnly(Side.CLIENT)
-    public void registerIcons(IconRegister par1IconRegister)
-    {
+    public void registerIcons(IconRegister par1IconRegister) {
         iconBuffer = new Icon[1];
         iconBuffer[0] = par1IconRegister.registerIcon("warpdrive:warpIsolation");
     }
 
     @Override
-    public Icon getIcon(int side, int metadata)
-    {
+    public Icon getIcon(int side, int metadata) {
         return iconBuffer[0];
     }
 
@@ -40,8 +38,7 @@ public class BlockWarpIsolation extends Block
      * Returns the quantity of items to drop on block destruction.
      */
     @Override
-    public int quantityDropped(Random par1Random)
-    {
+    public int quantityDropped(Random par1Random) {
         return 1;
     }
 
@@ -49,8 +46,7 @@ public class BlockWarpIsolation extends Block
      * Returns the ID of the items to drop on destruction.
      */
     @Override
-    public int idDropped(int par1, Random par2Random, int par3)
-    {
+    public int idDropped(int par1, Random par2Random, int par3) {
         return this.blockID;
     }
 }
