d8b98c433c9ccc81aed9720acb3df4acd2a33d64
Francesco Macagno <fjmacagno1@yahoo.com>
2015-07-20 00:12:38 -0700

    Updated commands

--- src/java/cr0s/warpdrive/command/InvisibleCommand.java
+++ src/java/cr0s/warpdrive/command/InvisibleCommand.java
@@ -3,45 +3,41 @@ package cr0s.warpdrive.command;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.server.MinecraftServer;
 
-public class InvisibleCommand extends CommandBase
-{
-    @Override
-    public int getRequiredPermissionLevel()
-    {
-        return 4;
-    }
+import com.sun.media.jfxmedia.logging.Logger;
 
-    @Override
-    public String getCommandName()
-    {
-        return "invisible";
-    }
+public class InvisibleCommand extends CommandBase {
+	@Override
+	public int getRequiredPermissionLevel() {
+		return 4;
+	}
 
-    @Override
-    public void processCommand(ICommandSender icommandsender, String[] astring)
-    {
-        EntityPlayerMP player = (EntityPlayerMP)icommandsender;
+	@Override
+	public String getCommandName() {
+		return "invisible";
+	}
 
-        if (astring.length >= 1)
-        {
-            notifyAdmins(icommandsender, "/invisible: setting invisible to " + astring[0], new Object[0]);
-            player = MinecraftServer.getServer().getConfigurationManager().getPlayerForUsername(astring[0]);
-        }
+	@Override
+	public void processCommand(ICommandSender icommandsender, String[] astring) {
+		EntityPlayerMP player = (EntityPlayerMP) icommandsender;
 
-        if (player == null)
-        {
-            return;
-        }
+		if (astring.length >= 1) {
+			Logger.logMsg(Logger.INFO, "/invisible: setting invisible to " + astring[0]);
+			// player =
+			// MinecraftServer.getServer().getConfigurationManager().getPlayerForUsername(astring[0]);
+			// TODO: Fix
+		}
 
-        // Toggle invisibility
-        player.setInvisible(!player.isInvisible());
-    }
+		if (player == null) {
+			return;
+		}
 
-    @Override
-    public String getCommandUsage(ICommandSender icommandsender)
-    {
-        return "/invisible [player]";
-    }
+		// Toggle invisibility
+		player.setInvisible(!player.isInvisible());
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender icommandsender) {
+		return "/invisible [player]";
+	}
 }
--- src/java/cr0s/warpdrive/command/JumpgateCommand.java
+++ src/java/cr0s/warpdrive/command/JumpgateCommand.java
@@ -3,32 +3,28 @@ package cr0s.warpdrive.command;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.ChatComponentText;
 import cr0s.warpdrive.WarpDrive;
 
-public class JumpgateCommand extends CommandBase
-{
-    @Override
-    public int getRequiredPermissionLevel()
-    {
-        return 4;
-    }
-
-    @Override
-    public String getCommandName()
-    {
-        return "jumpgates";
-    }
+public class JumpgateCommand extends CommandBase {
+	@Override
+	public int getRequiredPermissionLevel() {
+		return 4;
+	}
 
 	@Override
-	public String getCommandUsage(ICommandSender icommandsender)
-	{
+	public String getCommandName() {
+		return "jumpgates";
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender icommandsender) {
 		return "Lists jumpgates";
 	}
 
 	@Override
-	public void processCommand(ICommandSender icommandsender, String[] astring)
-	{
-		EntityPlayerMP player = (EntityPlayerMP)icommandsender;
-		player.addChatMessage(WarpDrive.jumpgates.commaList());
+	public void processCommand(ICommandSender icommandsender, String[] astring) {
+		EntityPlayerMP player = (EntityPlayerMP) icommandsender;
+		player.addChatMessage(new ChatComponentText(WarpDrive.jumpgates.commaList()));
 	}
 }
