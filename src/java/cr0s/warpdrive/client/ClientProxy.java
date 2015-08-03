8fc36a5b05fa579ff0277792fe40c3551ab8be5e
LemADEC <lemadec.fr@gmail.com>
2015-08-03 03:57:41 +0200

    Code cleanup

--- src/java/cr0s/warpdrive/client/ClientProxy.java
+++ src/java/cr0s/warpdrive/client/ClientProxy.java
@@ -1,10 +1,6 @@
 package cr0s.warpdrive.client;
 
-import net.minecraft.world.World;
-import cpw.mods.fml.client.FMLClientHandler;
 import cr0s.warpdrive.CommonProxy;
-import cr0s.warpdrive.data.Vector3;
-import cr0s.warpdrive.render.FXBeam;
 
 public class ClientProxy extends CommonProxy {
     @Override
