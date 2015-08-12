25573500e7d70b94df561513fa7d4f101a40cb5b
Francesco Macagno <fjmacagno1@yahoo.com>
2015-08-11 20:19:16 -0700

    Add planetoid to represent planets

---
+++ src/main/java/cr0s/warpdrive/conf/structures/Planetoid.java
@@ -0,0 +1,10 @@
+package cr0s.warpdrive.conf.structures;
+
+public class Planetoid extends Orb {
+	
+	public Planetoid(int diameter) {
+		super(diameter);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
