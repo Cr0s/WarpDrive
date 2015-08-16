5cf5d26fe16fd8babc4b0828e35968ed04065231
Francesco Macagno <fjmacagno1@yahoo.com>
2015-08-15 21:10:18 -0700

    Added GasCloud

---
+++ src/main/java/cr0s/warpdrive/conf/structures/GasCloud.java
@@ -0,0 +1,15 @@
+package cr0s.warpdrive.conf.structures;
+
+import java.awt.Color;
+
+public class GasCloud extends Orb {
+
+	private Color color;
+	
+	public GasCloud(int diameter, Color color) {
+		super(diameter);
+
+		this.color = color;
+	}
+
+}
