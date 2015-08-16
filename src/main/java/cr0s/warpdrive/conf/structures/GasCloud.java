9f3c4d61df16f200621bb974f846eb0b6cd24c85
Francesco Macagno <fjmacagno1@yahoo.com>
2015-08-15 21:17:30 -0700

    Added GasCloud

--- src/main/java/cr0s/warpdrive/conf/structures/GasCloud.java
+++ src/main/java/cr0s/warpdrive/conf/structures/GasCloud.java
@@ -1,6 +1,13 @@
 package cr0s.warpdrive.conf.structures;
 
 import java.awt.Color;
+import java.util.Random;
+
+import net.minecraft.block.Block;
+
+import org.w3c.dom.Element;
+
+import cr0s.warpdrive.WarpDrive;
 
 public class GasCloud extends Orb {
 
@@ -12,4 +19,13 @@ public class GasCloud extends Orb {
 		this.color = color;
 	}
 
+	@Override
+	public void loadFromXmlElement(Element e) {
+		//Not supported
+	}
+
+	@Override
+	public Block getBlockForRadius(Random r, int radius) {
+		return WarpDrive.blockGas; //TODO: Add color support
+	}
 }
