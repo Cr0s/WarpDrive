94684560f644d36c3f4777666e03d6ddf3bca043
Francesco Macagno <fjmacagno1@yahoo.com>
2015-08-16 23:17:33 -0700

    Add name option to orb

--- src/main/java/cr0s/warpdrive/conf/structures/Orb.java
+++ src/main/java/cr0s/warpdrive/conf/structures/Orb.java
@@ -19,31 +19,42 @@ import cr0s.warpdrive.conf.XmlRepresentable;
 import cr0s.warpdrive.world.EntitySphereGen;
 
 public abstract class Orb extends DeployableStructure implements XmlRepresentable {
-	
+
 	private OrbShell[] shellRelative;
 	private ArrayList<OrbShell> shells;
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 	public Orb(int diameter) {
 		super(diameter, diameter, diameter);
-		
+
 	}
-	
+
 	@Override
 	public void loadFromXmlElement(Element e) throws InvalidXmlException {
-		
+
+		this.name = e.getAttribute("name");
+
 		NodeList shells = e.getElementsByTagName("shell");
 		for (int i = 0; i < shells.getLength(); i++) {
 			Element tmp = (Element) shells.item(i);
-			
+
 			OrbShell shell = new OrbShell();
 			shell.loadFromXmlElement(tmp);
-			
+
 			setShell(shell.getRadiusInner(), shell.getRadiusOuter(), shell);
-			
+
 		}
-		
+
 	}
-	
+
 	@Override
 	public void saveToXmlElement(Element e, Document d) {
 		for (OrbShell shell : shells) {
@@ -51,9 +62,9 @@ public abstract class Orb extends DeployableStructure implements XmlRepresentabl
 			shell.saveToXmlElement(tmp, d);
 			e.appendChild(tmp);
 		}
-		
+
 	}
-	
+
 	@Override
 	public boolean generate(World world, Random p_76484_2_, int x, int y, int z) {
 		EntitySphereGen entitySphereGen = new EntitySphereGen(world, x, y, z, getHeight() / 2, this, true);
@@ -65,42 +76,42 @@ public abstract class Orb extends DeployableStructure implements XmlRepresentabl
 		world.spawnEntityInWorld(entitySphereGen);
 		return false;
 	}
-	
+
 	public void setShell(int inner, int outer, OrbShell shell) {
 		for (; inner <= outer; inner++)
 			shellRelative[inner] = shell;
 	}
-	
+
 	public OrbShell getShellForRadius(int r) {
 		return shellRelative[r];
 	}
-	
+
 	public Block getBlockForRadius(Random rand, int r) {
 		return shellRelative[r].getNextBlock(rand);
-		
-	}
-	
-	public class OrbShell implements XmlRepresentable {
-		
-		private int radiusInner, radiusOuter;
-		
-		private HashMap<String, Integer> compositionBlocks;
-		
-		private Block[] blockWeights;
-		
-		private String name;
-		
-		private int totalWeight;
 
-		private double genericOreChance;
+	}
+
+	public class OrbShell implements XmlRepresentable {
+
+		private int radiusInner, radiusOuter;
+
+		private HashMap<String, Integer> compositionBlocks;
+
+		private Block[] blockWeights;
+
+		private String name;
+
+		private int totalWeight;
 		
+		private double genericOreChance;
+
 		public OrbShell() {
 			compositionBlocks = new HashMap<String, Integer>();
 			totalWeight = 1;
 			blockWeights = new Block[0];
 			genericOreChance = 0;
 		}
-		
+
 		/**
 		 * Gets a block randomly chosen according to the setup of this OrbShell
 		 *
@@ -111,146 +122,146 @@ public abstract class Orb extends DeployableStructure implements XmlRepresentabl
 		public Block getNextBlock(Random r) {
 			return blockWeights[r.nextInt(totalWeight)];
 		}
-		
+
 		public int getRadiusInner() {
 			return radiusInner;
 		}
-		
+
 		public void setRadiusInner(int radiusInner) {
-			
+
 			this.radiusInner = radiusInner;
 		}
-		
+
 		public int getRadiusOuter() {
 			return radiusOuter;
 		}
-		
+
 		public void setRadiusOuter(int radiusOuter) {
 			this.radiusOuter = radiusOuter;
 		}
-		
+
 		@Override
 		public void loadFromXmlElement(Element e) throws InvalidXmlException {
-			
+
 			WarpDrive.logger.info("Loading shell " + e.getAttribute("name"));
 			name = e.getAttribute("name");
-			
+
 			totalWeight = 0;
-			
+
 			try {
 				radiusInner = Integer.parseInt(e.getAttribute("radiusInner"));
 				radiusOuter = Integer.parseInt(e.getAttribute("radiusOuter"));
-				
+
 				if (radiusInner < 1 || radiusInner > radiusOuter)
 					throw new InvalidXmlException("Orb creation arguments are incorrect!");
-				
+
 				NodeList compBlocks = e.getElementsByTagName("CompositionBlock");
 				for (int i = 0; i < compBlocks.getLength(); i++) {
 					Element tmp = (Element) compBlocks.item(i);
-					
+
 					if (!tmp.hasAttribute("weight"))
 						throw new InvalidXmlException("Shell is missing weight at place " + i + "!");
-					
+
 					if (!tmp.hasAttribute("block"))
 						throw new InvalidXmlException("Shell is missing block at place " + i + "!");
-					
+
 					int tmpWeight = Integer.parseInt(tmp.getAttribute("weight"));
-					
+
 					if (tmpWeight < 1)
 						throw new InvalidXmlException("Weight is less than 1 at place " + i + "!");
-					
+
 					String tmpBlock = tmp.getAttribute("block");
-					
+
 					if (Block.getBlockFromName(tmpBlock) == null)
 						throw new InvalidXmlException("Shell has unknown block at place " + i + "!");
-					
+
 					totalWeight += tmpWeight;
-					
+
 					compositionBlocks.put(tmpBlock, tmpWeight);
 				}
-
-				HashMap<String, Integer> categoryTemp = new HashMap<String, Integer>();
 				
+				HashMap<String, Integer> categoryTemp = new HashMap<String, Integer>();
+
 				NodeList oreCategories = e.getElementsByTagName("OreCategory");
 				for (int i = 0; i < oreCategories.getLength(); i++) {
 					Element cat = (Element) oreCategories.item(i);
-					
+
 					if (!cat.hasAttribute("weight"))
 						throw new InvalidXmlException("OreCategory " + i + " must have a weight");
-					
+
 					if (!cat.hasAttribute("category"))
 						throw new InvalidXmlException("OreCategory " + i + " must have a category");
-					
+
 					int weight = Integer.parseInt(cat.getAttribute("weight"));
 					String oreSpawnCategory = cat.getAttribute("category");
-
+					
 					if (OreSpawnCategory.getOreSpawnValue(oreSpawnCategory) == null)
 						throw new InvalidXmlException("Shell has an invalid ores category!");
-					
+
 					if (weight < 1)
 						throw new InvalidXmlException("Shell has an invalid ores probability!");
-
+					
 					totalWeight += weight;
-
+					
 					if(categoryTemp.containsKey(oreSpawnCategory))
 						throw new InvalidXmlException("OreCategory used twice in same shell!");
-
+					
 					categoryTemp.put(oreSpawnCategory, weight);
-
+					
 				}
-				
+
 				int index = 0;
 				blockWeights = new Block[totalWeight];
-				
+
 				for (Entry<String, Integer> compBlock : compositionBlocks.entrySet()) {
-					
+
 					Block bl = Block.getBlockFromName(compBlock.getKey());
-					
+
 					for (int i = 0; i < compBlock.getValue(); i++) {
 						blockWeights[index++] = bl;
 					}
-					
+
 				}
-				
+
 				for(Entry<String, Integer> category : categoryTemp.entrySet()) {
 					OreSpawnCategory value = OreSpawnCategory.getOreSpawnValue(category.getKey());
 					for (Entry<Block, Integer> compBlock : value.getOres()) {
-						
+
 						int reduced = category.getValue() * compBlock.getValue() / value.getTotalWeight();
 						for (int i = 0; i < reduced; i++) {
 							blockWeights[index++] = compBlock.getKey();
 						}
-						
+
 					}
 				}
-
 				
+
 			} catch (NumberFormatException ex) {
 				throw new InvalidXmlException("Invalid integer in shell " + name + "!");
 			}
-			
+
 			if (totalWeight < 1)
 				throw new InvalidXmlException("At least one kind of block must be defined!");
-			
+
 		}
-		
+
 		@Override
 		public void saveToXmlElement(Element e, Document d) {
 			WarpDrive.logger.info("Saving shell " + e.getAttribute("name"));
-			
+
 			e.setAttribute("name", name);
 			e.setAttribute("radiusInner", "" + radiusInner);
 			e.setAttribute("radiusOuter", "" + radiusOuter);
-			
+
 			for (Entry<String, Integer> compBlock : compositionBlocks.entrySet()) {
 				Element child = d.createElement("CompositionBlock");
 				child.setAttribute("weight", compBlock.getValue() + "");
 				child.setAttribute("block", compBlock.getKey());
 				e.appendChild(child);
 			}
-			
+
 		}
-		
+
 	}
-	
+
 }
--- src/main/java/cr0s/warpdrive/conf/structures/StructureManager.java
+++ src/main/java/cr0s/warpdrive/conf/structures/StructureManager.java
@@ -18,45 +18,45 @@ import cr0s.warpdrive.conf.WarpDriveConfig;
 
 
 public class StructureManager {
-
+	
 	private static ArrayList<Star> stars = new ArrayList<Star>();
 	private static ArrayList<Planetoid> moons = new ArrayList<Planetoid>();
-
+	
 	public static void loadStructures(String structureConfDir) {
 		File dir = new File(structureConfDir);
-		
+
 		dir.mkdir();
-		
+
 		if (!dir.isDirectory()) {
 			throw new IllegalArgumentException("File path " + dir.getPath() + " must be a directory!");
 		}
-		
+
 		File[] files = dir.listFiles(new FilenameFilter() {
 			@Override
 			public boolean accept(File file_notUsed, String name) {
 				return name.startsWith("structure") && name.endsWith(".xml");
 			}
 		});
-		
+
 		for (File file : files) {
 			try {
-				
+
 				WarpDrive.logger.info("Loading structure data file " + file.getPath());
-				
+
 				loadXmlStructureFile(file);
-				
+
 				WarpDrive.logger.info("Finished loading structure data file " + file.getPath());
-				
+
 			} catch (Exception e) {
 				WarpDrive.logger.error("Error loading file " + file.getPath() + ": " + e.getMessage());
 				e.printStackTrace();
 			}
 		}
 	}
-	
+
 	private static void loadXmlStructureFile(File f) throws SAXException, IOException, InvalidXmlException {
 		Document base = WarpDriveConfig.getXmlDocumentBuilder().parse(f);
-		
+
 		NodeList modReqList = base.getElementsByTagName("ModRequirements");
 		for (int i = 0; i < modReqList.getLength(); i++) {
 			NodeList mods = ((Element) modReqList.item(i)).getElementsByTagName("mod");
@@ -65,33 +65,33 @@ public class StructureManager {
 				if (!mod.hasAttribute("name")) {
 					throw new InvalidXmlException("A mod requirement at " + i + ":" + j + " is missing the name attribute!");
 				}
-
+				
 				String name = mod.getAttribute("name");
 				if (!Loader.isModLoaded(name)) {
 					WarpDrive.logger.info("Skippping structure data file " + f.getPath() + " because the mod " + name + " is not loaded");
 				}
-				
+
 			}
 		}
-
+		
 		NodeList structures = base.getElementsByTagName("structure");
 		for (int i = 0; i < structures.getLength(); i++) {
-
+			
 			Element struct = (Element) structures.item(i);
-
+			
 			String type = struct.getAttribute("type");
 			String name = struct.getAttribute("name");
-
+			
 			if(type.isEmpty())
 				throw new InvalidXmlException("Structure must have a type!");
-
+			
 			int radius;
 			try {
 				radius = Integer.parseInt(struct.getAttribute("radius"));
 			} catch(NumberFormatException e) {
 				throw new InvalidXmlException("Structure radius is invalid!");
 			}
-
+			
 			if (type.equalsIgnoreCase("star")) {
 				Star s = new Star(radius);
 				s.loadFromXmlElement(struct);
@@ -102,7 +102,7 @@ public class StructureManager {
 			}
 		}
 	}
-
+	
 	public static DeployableStructure getStructure(Random random, final String name, final String type) {
 		if (name == null || name.length() == 0) {
 			if (type == null || type.length() == 0) {
@@ -114,24 +114,23 @@ public class StructureManager {
 			}
 		} else {
 			for (Star star : stars) {
-				// if (star.getName().equalsIgnoreCase(name)) { FIXME add a name property so we can refer to it
+				if (star.getName().equals(name))
 					return star;
-				// }
 			}
 		}
-		
+
 		// not found or nothing defined => return null
 		return null;
 	}
-
+	
 	public static DeployableStructure getStar(Random random, final String name) {
 		return getStructure(random, name, "star");
 	}
-
+	
 	public static DeployableStructure getMoon(Random random, final String name) {
 		return getStructure(random, name, "moon");
 	}
-
+	
 	public static DeployableStructure getGasCloud(Random random, final String name) {
 		return getStructure(random, name, "cloud");
 	}
