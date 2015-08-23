b3bd1b7695a660a4041dee8cb3d324f61d7ee97e
Francesco Macagno <fjmacagno1@yahoo.com>
2015-08-23 14:50:35 -0700

    Added SchematicStructure to eventually represent schematics

---
+++ src/main/java/cr0s/warpdrive/conf/structures/SchematicStructure.java
@@ -0,0 +1,20 @@
+package cr0s.warpdrive.conf.structures;
+
+import java.util.Random;
+
+import net.minecraft.world.World;
+
+public class SchematicStructure extends DeployableStructure {
+	
+	public SchematicStructure(int height, int width, int length) {
+		super(height, width, length);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public boolean generate(World p_76484_1_, Random p_76484_2_, int p_76484_3_, int p_76484_4_, int p_76484_5_) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
