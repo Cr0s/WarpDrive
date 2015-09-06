28f91fa125263a842b3caf1cf79386f2d52cadf1
LemADEC <lemadec.fr@gmail.com>
2015-09-06 22:12:11 +0200

    Code cleanup

--- src/main/java/cr0s/warpdrive/block/TileEntityAirGenerator.java
+++ src/main/java/cr0s/warpdrive/block/TileEntityAirGenerator.java
@@ -10,25 +10,25 @@ import cr0s.warpdrive.config.WarpDriveConfig;
 public class TileEntityAirGenerator extends TileEntityAbstractEnergy {
 	private int cooldownTicks = 0;
 	private final int START_CONCENTRATION_VALUE = 15;
-
+	
 	public TileEntityAirGenerator() {
 		super();
+		
 		peripheralName = "warpdriveAirGenerator";
-		// methodsArray = Arrays.asList("", "");;
 	}
 	
 	@Override
 	public void updateEntity() {
 		super.updateEntity();
-
+		
 		if (FMLCommonHandler.instance().getEffectiveSide().isClient()) {
 			return;
 		}
-
-		if (this.isInvalid()) {
+		
+		if (isInvalid()) {
 			return;
 		}
-
+		
 		// Air generator works only in spaces
 		if (worldObj.provider.dimensionId != WarpDriveConfig.G_SPACE_DIMENSION_ID && worldObj.provider.dimensionId != WarpDriveConfig.G_HYPERSPACE_DIMENSION_ID) {
 			if (getBlockMetadata() != 0) {
@@ -36,7 +36,7 @@ public class TileEntityAirGenerator extends TileEntityAbstractEnergy {
 			}
 			return;
 		}
-
+		
 		cooldownTicks++;
 		if (cooldownTicks > WarpDriveConfig.AIRGEN_AIR_GENERATION_TICKS) {
 			if (consumeEnergy(WarpDriveConfig.AIRGEN_ENERGY_PER_NEWAIRBLOCK, true)) {
@@ -54,11 +54,11 @@ public class TileEntityAirGenerator extends TileEntityAbstractEnergy {
 			releaseAir(0, -1, 0);
 			releaseAir(0, 0, 1);
 			releaseAir(0, 0, -1);
-
+			
 			cooldownTicks = 0;
 		}
 	}
-
+	
 	private void releaseAir(int xOffset, int yOffset, int zOffset) {
 		Block block = worldObj.getBlock(xCoord + xOffset, yCoord + yOffset, zCoord + zOffset);
 		if (block.isAir(worldObj, xOffset, yOffset, zOffset)) {// can be air
@@ -78,29 +78,28 @@ public class TileEntityAirGenerator extends TileEntityAbstractEnergy {
 					} else if (metadata > 1) {
 						worldObj.setBlockMetadataWithNotify(xCoord + xOffset, yCoord + yOffset, zCoord + zOffset, 1, 2);
 					} else {
-						// worldObj.setBlock(xCoord + xOffset, yCoord + yOffset,
-						// zCoord + zOffset, 0, 0, 2);
+						// worldObj.setBlockMetadataWithNotify(xCoord + xOffset, yCoord + yOffset,  zCoord + zOffset, 0, 0, 2);
 					}
 				}
 			}
 		}
 	}
-
+	
 	@Override
 	public void readFromNBT(NBTTagCompound tag) {
 		super.readFromNBT(tag);
 	}
-
+	
 	@Override
 	public void writeToNBT(NBTTagCompound tag) {
 		super.writeToNBT(tag);
 	}
-
+	
 	@Override
 	public int getMaxEnergyStored() {
 		return WarpDriveConfig.AIRGEN_MAX_ENERGY_STORED;
 	}
-
+	
 	@Override
 	public boolean canInputEnergy(ForgeDirection from) {
 		return true;
